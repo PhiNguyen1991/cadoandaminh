287d39059b959a69cd3860dcd4c8f7014f4746fe
phi.nguyen <mrdragon1415@gmail.com>
2020-06-29 11:36:07 +0700

    Updates

---
+++ precache-manifest.7beccad21092bbecfc6c9965943dc27e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0395bd24564e6d91c34e02d768692c",
+    "url": "/cadoandaminh/index.html"
+  },
+  {
+    "revision": "92d06f61ac9ac6375d84",
+    "url": "/cadoandaminh/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cadoandaminh/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "92d06f61ac9ac6375d84",
+    "url": "/cadoandaminh/static/js/main.01616f17.chunk.js"
+  },
+  {
+    "revision": "0fe9902549121d29cc07",
+    "url": "/cadoandaminh/static/js/runtime~main.e6173589.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cadoandaminh/static/media/logo.5d5d9eef.svg"
+  }
+]);
